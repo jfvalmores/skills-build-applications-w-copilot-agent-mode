8a73ee5bea076f0b53112f56a022e30565370e45
Justine Valmores <jfvalmores@gmail.com>
2026-03-05 05:30:17 +0000

    Update settings and URLs to support dynamic ALLOWED_HOSTS and API root URL for Codespaces

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-569vc=#^*d2!swl0fed=mxn!grmq)1#^()x9q2e8m5=jk50vn+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
